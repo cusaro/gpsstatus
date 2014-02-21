0b4c376197a77404e96f63c52c512ff83a960029
Alexander Rothe <alexander.rothe@mynewsdesk.com>
2014-02-21 13:16:23 +0100

    another try to fix it

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,9 @@ set :keep_releases, 5
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :bundle_binstubs, false
+set :bundle_flags, ''
+set :bundle_binstubs, nil
+set :bundle_dir, nil
 
 # what specs should be run before deployment is allowed to# continue, see lib/capistrano/tasks/run_tests.cap
 set :tests, ["spec"]
