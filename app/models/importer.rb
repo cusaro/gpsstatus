5bae644c2b7c5165eed192dbc34b423c3341e165
Alexander Rothe <alexander.rothe@mynewsdesk.com>
2014-02-22 22:14:26 +0100

    Move Import Code to new Class

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -1,52 +1,35 @@
-require 'gpx'
-
 class Importer
-  IMPORT_DIR = 'public/system/gpxfiles/import/'
-  GPX_DIR = IMPORT_DIR
-  THUMBNAIL_DIR = 'app/assets/images/thumbnails'
 
-  def import
-    filelist.each do |file|
+  def save(file)
+    gpx = GPX::GPX.new file
 
-      next if file_exists?(file)
-
-      gpx = GPX::GPX.new file
-
-      GpsFile.create(
-          name: File.basename(file),
-          duration: gpx.duration,
-          length: gpx.length,
-          average_speed: gpx.average_speed,
-          start: gpx.start_date,
-          end: gpx.end_date,
-          image: File.open(image(file)),
-          country: geocode(gpx).country,
-          city: geocode(gpx).city,
-          filename: File.basename(file),
-      )
-
-    end
-  end
-
-  def filelist
-    Dir[IMPORT_DIR+"*.gpx"].reject{ |f| f[%r{.*_[0-9][0-9][0-9].gpx}]  }
-  end
-
-  def file_exists?(file)
-    GpsFile.exists?(filename: File.basename(file))
+    GpsFile.create(
+        name: File.basename(file),
+        duration: gpx.duration,
+        length: gpx.length,
+        average_speed: gpx.average_speed,
+        start: gpx.start_date,
+        end: gpx.end_date,
+        image: File.open(image(file)),
+        country: geocode(gpx).country,
+        city: geocode(gpx).city,
+        filename: File.basename(file),
+    )
   end
 
   def geocode(gpx)
-    Geocode.new(gpx)
+    @geocode ||= Geocode.new(gpx)
   end
 
   def image(file)
-    i = Image.new(file)
-    i.create_image
+
+    if @image.present?
+      @image
+    else
+      @image = Image.new(file)
+      @image.create_image
+    end
+
   end
 
 end
-
-
-#rails runner -e prodction "Importer.import"
-#rake import
