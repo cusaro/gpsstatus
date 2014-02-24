3199da0b340ae05aae26f507b6b02d70d3bc3e73
Alexander Rothe <alexander.rothe@mynewsdesk.com>
2014-02-24 11:01:28 +0100

    Importer now uses Tags

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -1,20 +1,21 @@
 class Importer
 
   def save(file)
-    gpx = GPX::GPX.new file
+    gpx_file = GPX::GPX.new file
 
-    GpsFile.create(
+    gpx = GpsFile.create(
         name: File.basename(file),
-        duration: gpx.duration,
-        length: gpx.length,
-        average_speed: gpx.average_speed,
-        start: gpx.start_date,
-        end: gpx.end_date,
+        duration: gpx_file.duration,
+        length: gpx_file.length,
+        average_speed: gpx_file.average_speed,
+        start: gpx_file.start_date,
+        end: gpx_file.end_date,
         image: File.open(image(file)),
-        country: geocode(gpx).country,
-        city: geocode(gpx).city,
         filename: File.basename(file),
     )
+
+    gpx.tags << Tag.new(name:geocode(gpx_file).city)
+    gpx.tags << Tag.new(name:geocode(gpx_file).country)
   end
 
   def geocode(gpx)
