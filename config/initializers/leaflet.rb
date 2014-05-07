1c1238e32a2861476d86aba1940b6158796568c7
Alexander Rothe <alexander.rothe@mynewsdesk.com>
2014-05-07 22:53:32 +0200

    change map provider to 4umaps.eu

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,4 +1,5 @@
-Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/ac15c1310d31495a9b17aaf8b9c11526/997/256/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "http://www.4umaps.eu/{z}/{x}/{y}.png"
+
 # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
-Leaflet.attribution = "gps.alaexander-rothe.de"
-Leaflet.max_zoom = 18
+Leaflet.attribution = "Karte von: www.4umaps.eu"
+Leaflet.max_zoom = 15
