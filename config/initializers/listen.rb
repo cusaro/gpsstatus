0e7a7365ca1b312429e2be645d719f6e239614a6
Alexander Rothe <alexander.rothe@mynewsdesk.com>
2014-09-01 09:32:47 +0200

    remove Thread from listen initializer

--- config/initializers/listen.rb
+++ config/initializers/listen.rb
@@ -1,19 +1,17 @@
 require 'gpx'
 
-Thread.new do
 
-  if Rails.env.production?
-    path = '/var/webapps/gpsstats/shared/public/system/gpxfiles/import/'
-  else
-    path = 'public/system/gpxfiles/import/'
-  end
+if Rails.env.production?
+  path = '/var/webapps/gpsstats/shared/public/system/gpxfiles/import/'
+else
+  path = 'public/system/gpxfiles/import/'
+end
 
-  listener = Listen.to(path,wait_for_delay: 30 ) do |modified, added, removed|
-    unless added.empty?
-      added.each do |file|
-        Importer.new(file).save
-      end
+listener = Listen.to(path) do |modified, added, removed|
+  unless added.empty?
+    added.each do |file|
+      Importer.new(file).save
     end
   end
-  listener.start # not blocking
 end
+listener.start # not blocking
